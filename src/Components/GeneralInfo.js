3970a0b5ed8f3b746d2ee8f8514a84a80e3d4ae2
HaMza HashMi <73049209+MohammadHamzaMushtaqhashmi@users.noreply.github.com>
2023-07-12 11:38:44 +0500

    Update GeneralInfo.js
    convert it into functional component

--- src/Components/GeneralInfo.js
+++ src/Components/GeneralInfo.js
@@ -1,72 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
 
-class GeneralInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      isEditing: true
-    };
-  }
+const GeneralInfo = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [isEditing, setIsEditing] = useState(true);
 
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const target = event.target;
     const name = target.name;
     const value = target.value;
 
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.setState({ isEditing: false });
-  }
-
-  handleEdit = () => {
-    this.setState({ isEditing: true });
-  }
-
-  render() {
-    if (this.state.isEditing) {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <h2>General Information</h2>
-          <label>
-            Name:
-            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Email:
-            <input type="email" name="email" value={this.state.email} onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Phone Number:
-            <input type="tel" name="phone" value={this.state.phone} onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-          <button type="button" onClick={this.handleEdit}>Edit</button>
-        </form>
-      );
-    } else {
-      return (
-        <div>
-          <h2>General Information</h2>
-          <p>Name: {this.state.name}</p>
-          <p>Email: {this.state.email}</p>
-          <p>Phone Number: {this.state.phone}</p>
-          <button type="button" onClick={this.handleEdit}>Edit</button>
-        </div>
-      );
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'phone') {
+      setPhone(value);
     }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsEditing(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  if (isEditing) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>General Information</h2>
+        <label>
+          Name:
+          <input type="text" name="name" value={name} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={email} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input type="tel" name="phone" value={phone} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        <button type="button" onClick={handleEdit}>Edit</button>
+      </form>
+    );
+  } else {
+    return (
+      <div>
+        <h2>General Information</h2>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone Number: {phone}</p>
+        <button type="button" onClick={handleEdit}>Edit</button>
+      </div>
+    );
   }
-}
+};
 
 export default GeneralInfo;
